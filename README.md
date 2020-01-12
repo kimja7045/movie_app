75d0c037fca09ae6851336593728d947a6d07a08
kimja7045 <kimja7045@naver.com>
2020-01-12 16:47:03 +0900

    ~Props, PropTypes

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 # Movie App 2019
 
 React JS Fundamentals Course (2019 Update!)
+
+# Protection with PropTypes - #2.4
+npm i prop-types
--- src/App.js
+++ src/App.js
@@ -1,11 +1,48 @@
 import React from 'react';
-import './App.css';
+import PropTypes from "prop-types";
+
+// function Food({name,name2,rating}) {
+//   // return <h1>Hi~I like {name}</h1>
+//   return(
+//     <div>
+//       <h1>I Like {name}</h1>
+//       <h4>{rating}/5.0</h4>
+//       <h2>나는 {name2}을 좋아한다</h2>
+//     </div>
+//   );
+// }
+
+// Food.propTypes={
+//   name:PropTypes.string.isRequired,
+//   name2:PropTypes.string.isRequired,
+//   rating:PropTypes.number.isRequired
+// };
+
+// const foodLike = [
+//   { name: "Kimchi",
+//     name2:"김치",
+//     rating:5.3
+//   }, 
+//   { name: "kimbab",
+//     name2:"김밥",
+//     rating:5.5
+//   }
+// ];
+
+// function renderfood(dish){
+//   //console.log(dish);  
+//   return <Food name={dish.name} name2={dish.name2} rating={dish.rating} key={dish.name}/>
+// }
 
 function App() {
   return (
-    <div className="App">
-      
-    </div>
+    <div>
+      {/* {FoodLike.map((dish) => (
+        <food name={dish.name}
+              key={dish.name}/>
+      ))} */}
+      {/* {foodLike.map(renderfood)} */}
+    </div >
   );
 }
 
