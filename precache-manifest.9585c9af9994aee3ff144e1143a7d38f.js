8d9fe56bf8637bd06dce701ef69dd76a6cf22c3e
kimja7045 <kimja7045@naver.com>
2020-01-19 13:30:41 +0900

    Updates

---
+++ precache-manifest.9585c9af9994aee3ff144e1143a7d38f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0875afde01131bf5ecdd7b55e6ea8705",
+    "url": "/movie_app/index.html"
+  },
+  {
+    "revision": "7e44a9c5a627b23d7798",
+    "url": "/movie_app/static/css/main.0e97a636.chunk.css"
+  },
+  {
+    "revision": "329dfed09cd4ed43b438",
+    "url": "/movie_app/static/js/2.7665f162.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/movie_app/static/js/2.7665f162.chunk.js.LICENSE"
+  },
+  {
+    "revision": "7e44a9c5a627b23d7798",
+    "url": "/movie_app/static/js/main.a8aa2505.chunk.js"
+  },
+  {
+    "revision": "5c8eae3b718945eb5d78",
+    "url": "/movie_app/static/js/runtime-main.064eeeaa.js"
+  }
+]);
